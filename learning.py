772aa8d3c659e51e1fdf31ea0f26904fafa4177d
shuos-rst <54552425+shuos-rst@users.noreply.github.com>
2021-03-09 11:49:07 -0600

    more dice stuff

--- learning.py
+++ learning.py
@@ -1,20 +1,22 @@
-# we out here learning
 import random
+import sys
 
 def dn(n): #a die with n sides
-    if (n > 10000) or (n <= 0): #edge case handling. fix this to have proper try/except handling
-        return 
-    die = 1/n
-    a = random.random()
-    return (int((a // die) + 1))
+    return random.randrange(1, n+1)
 
 def xdn(x, n): #x dice with n sides
-    out = 0
+    total = 0
+    dice = []
     for i in range(x):
-        out = out + dn(n)
-    return(out)
+        ret = dn(n)
+        total = total + ret
+        dice.append(ret)
+    string_ints = [str(int) for int in dice] #this creates a new variable string_ints, and initializes it by taking every int in dice and converting it to a string
+    return("Sum: " + str(total) + ". Dice: " + ", ".join(string_ints))
 
 
+x = int(sys.argv[1]) #sys.argv[] is the inputs from the commmand line
+n = int(sys.argv[2])
+print(xdn(x,n))
 
-for x in range (1):
-    print(xdn(2,0))
+#todo: implement edge case handling
